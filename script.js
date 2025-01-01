1d69f9c82cec2f4de7451bb7a4d5a279b8cd445a
Wajsar Josef <36114753+PepikVaio@users.noreply.github.com>
2025-01-01 17:01:48 +0100

    Přidání podpory pro české znaky pomocí vlastního fontu

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-const { PDFDocument } = require('pdf-lib');
+const { PDFDocument, rgb } = require('pdf-lib');
 const fs = require('fs');
 
 async function createPDF() {
+  // Načti vlastní font
+  const fontBytes = fs.readFileSync('DejaVuSans.ttf');
+
   const pdfDoc = await PDFDocument.create();
   const page = pdfDoc.addPage([600, 400]);
-  page.drawText('Ahoj světe z pdf-lib!', { x: 50, y: 350 });
+
+  // Vlož vlastní font
+  const customFont = await pdfDoc.embedFont(fontBytes);
+
+  // Použij vlastní font pro text
+  page.drawText('Ahoj světe z pdf-lib! Čeština: ěščřžýáíé', {
+    x: 50,
+    y: 350,
+    size: 24,
+    font: customFont,
+    color: rgb(0, 0, 0),
+  });
 
   const pdfBytes = await pdfDoc.save();
   fs.writeFileSync('output.pdf', pdfBytes);
